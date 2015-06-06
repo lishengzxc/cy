63c6e6c99e8e69cd693336491a718ee5a0495331
lishengzxc <eric@lishengcn.cn>
2015-06-06 13:46:02 +0800

    save

--- js/app.js
+++ js/app.js
@@ -39,7 +39,21 @@ cy.directive('menusitemsbox', function() {
 	return {
 		restrice: 'E',
 		templateUrl: 'tpls/menus-items-box.html',
-		replace: true
+		replace: true,
+		link: function(s, e, a) {
+			var startY, endY;
+			e.bind('touchstart', function(e) {
+				var touch = e.touches[0];
+				startY = touch.pageY;
+				//console.log(startY);
+			});
+			e.bind('touchmove', function(e) {
+				var touch = e.touches[0];
+				endY = (startY - touch.pageY) * -1;
+				//console.log(endY);
+				console.log(document.getElementsByClassName('menus-items-box')[0].style.transform = 'translateY(' + endY + 'px)');
+			});
+		}
 	}
 });
 
