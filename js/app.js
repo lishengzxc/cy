7bc68c5fa3be1b48d1ec2886c680ebc8236e99f1
lishengzxc <eric@lishengcn.cn>
2015-06-09 22:31:07 +0800

    update

--- js/app.js
+++ js/app.js
@@ -75,6 +75,139 @@ cy.directive('poppanel', function () {
 	}
 });
 
+cy.directive('order', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'tpls/order.html',
+		replace: true
+	}
+});
+
+cy.directive('cartrecommend', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'tpls/cart_recommend.html',
+		replace: true,
+		link: function(s, e, a, c) {
+			// 左右侧滑效果
+			e[0].children[0].style.display = 'block';
+			for (var i = 1; i < e[0].children.length - 1; i++) {
+				e[0].children[i].style.display = 'none';
+			}
+			var index = 0;
+			var animationFlag = true;
+			var startX = 0;
+			var direction = 'left';
+
+			var slideStep1 = function(index, s, d) {
+				s[index].style.display = 'block';
+				if (d === 'left') {
+					s[index].classList.add('slide', 'out');
+					if (index !== s.length - 2) {
+						s[index + 1].style.display = 'block';
+						s[index + 1].classList.add('in', 'slide');
+					} else {
+						s[0].style.display = 'block';
+						s[0].classList.add('in', 'slide');
+					}
+				} else {
+					s[index].classList.add('slide', 'out', 'reverse');
+					if (index !== 0) {
+						s[index - 1].style.display = 'block';
+						s[index - 1].classList.remove('out');
+						s[index - 1].classList.add('in', 'slide', 'reverse');
+					} else {
+						s[s.length - 2].style.display = 'block';
+						s[s.length - 2].classList.remove('out');
+						s[s.length - 2].classList.add('in', 'slide', 'reverse');
+					}
+				}
+			};
+
+			var slideStpe2 = function(index, s, d) {
+				//s[index].classList.remove('slide', 'reverse');
+				s[index].classList.remove('slide', 'out');
+				s[index].style.display = 'none';
+				s[index + 1].classList.remove('slide', 'in');
+				s[index + 1].style.display = 'block';
+
+				if (d === 'left') {
+					if (index !== s.length - 2) {
+						s[index + 1].classList.remove('slide');
+					} else {
+						s[0].classList.remove('slide');
+					}
+				} else {
+					if (index !== 0) {
+						s[index - 1].classList.remove('slide', 'reverse');
+					} else {
+						s[s.length - 2].classList.remove('slide', 'reverse');
+					}
+				}
+			};
+
+			var moveIndex = function(index, s, d) {
+				s[s.length - 1].children[index].classList.remove('current');
+				if (d === 'left') {
+					var ti = index === s.length - 2 ? 0 : index + 1;
+					s[s.length - 1].children[ti].classList.add('current');
+				} else {
+					var ti = index === 0 ? s.length - 2 : index - 1;
+					s[s.length - 1].children[ti].classList.add('current');
+				}
+			};
+
+			var slide = function(event) {
+				if (!event) {
+					direction = 'left';
+				} else {
+					if (startX - event.touches[0].pageX > 0) {
+						direction = 'left'
+					} else {
+						direction = 'right'
+					}
+				}
+				if (animationFlag) {
+					animationFlag = false;
+					var s = e[0].children;
+					index = 0;
+					for (var i = 0; i < s.length - 1; i++) {
+						if (s[i].classList.contains('in')) {
+							index = i;
+							break;
+						}
+					}
+					slideStep1(index, s, direction);
+					setTimeout(function() {
+						slideStpe2(index, s, direction);
+						moveIndex(index, s, direction);
+						animationFlag = true;
+					}, 350);
+				}
+			};
+			e.on('touchstart', function(event) {
+				startX = event.touches[0].pageX;
+			});
+			e.on('touchmove', slide);
+			//e.on('webkitAnimationEnd', function(event) {
+			//	var s = e[0].children;
+			//	slideStpe2(index, s, direction);
+			//	moveIndex(index, s, direction);
+			//	animationFlag = true;
+			//});
+			//e.on('animationend', function(event) {
+			//	var s = e[0].children;
+			//	slideStpe2(index, s, direction);
+			//	moveIndex(index, s, direction);
+			//	animationFlag = true;
+			//});
+			setInterval(slide, 2000);
+
+			// 点击添加
+
+		}
+	}
+});
 
 
 // 以下为各控制器
@@ -84,6 +217,10 @@ cy.controller('cyController', ['$rootScope', function($rootScope) {
 	$rootScope.meunsDisplay = true;
 	$rootScope.cartDisplay = false;
 	$rootScope.noitemsDisplay = true;
+
+	//$rootScope.meunsDisplay = false;
+	//$rootScope.cartDisplay = true;
+	//$rootScope.noitemsDisplay = false;
 
 	$rootScope.toCart = function() {
 
@@ -110,9 +247,35 @@ cy.controller('cyController', ['$rootScope', function($rootScope) {
 		this.cartDisplay = false;
 	};
 
+	$rootScope.nowClassify = '1';
+
+	$rootScope.classifySets = [
+		{
+			typeid: 1,
+			typename: "热"
+		},
+		{
+			typeid: 2,
+			typename: "冷"
+		},
+		{
+			typeid: 3,
+			typename: "饮"
+		},
+		{
+			typeid: 4,
+			typename: "它"
+		}
+	];
+
+	$rootScope.jumpSomeClass = function($event) {
+		this.nowClassify = $event.target.id;
+	};
+
 	$rootScope.resMenus = [
 		{
 			id: 1,
+			type: 1,
 			name: '1号菜',
 			info: '一些其他信息',
 			sales: '99',
@@ -121,6 +284,7 @@ cy.controller('cyController', ['$rootScope', function($rootScope) {
 		},
 		{
 			id: 2,
+			type: 1,
 			name: '2号菜',
 			info: '一些其他信息',
 			sales: '99',
@@ -129,6 +293,7 @@ cy.controller('cyController', ['$rootScope', function($rootScope) {
 		},
 		{
 			id: 3,
+			type: 3,
 			name: '3号菜',
 			info: '一些其他信息',
 			sales: '99',
@@ -137,6 +302,7 @@ cy.controller('cyController', ['$rootScope', function($rootScope) {
 		},
 		{
 			id: 4,
+			type: 4,
 			name: '123123',
 			info: '一些其他信息',
 			sales: '99',
@@ -145,6 +311,7 @@ cy.controller('cyController', ['$rootScope', function($rootScope) {
 		},
 		{
 			id: 5,
+			type: 4,
 			name: 'sdfasdf',
 			info: '一些其他信息',
 			sales: '99',
@@ -153,135 +320,8 @@ cy.controller('cyController', ['$rootScope', function($rootScope) {
 		},
 		{
 			id: 6,
-			name: 'ewr',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 7,
-			name: 'dsfdfv',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 8,
-			name: 'dsfdfv',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 9,
-			name: 'dsfdfv',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 10,
-			name: 'dsfdfv',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 11,
-			name: 'dsfdfv',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 12,
-			name: 'dsfdfv',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 13,
-			name: 'dsfdfv',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 14,
-			name: 'dsfdfv',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 15,
-			name: 'dsfdfv',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 16,
-			name: 'dsfdfv',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 17,
-			name: 'dsfdfv',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 18,
-			name: 'dsfdfv',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 19,
-			name: 'dsfdfv',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 20,
-			name: 'dsfdfv',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 21,
-			name: 'dsfdfv',
-			info: '一些其他信息',
-			sales: '99',
-			num: 0,
-			price: '50.0'
-		},
-		{
-			id: 22,
-			name: 'dsfdfv',
+			type: 2,
+			name: '凉拌猪耳',
 			info: '一些其他信息',
 			sales: '99',
 			num: 0,
@@ -351,7 +391,6 @@ cy.controller('cyController', ['$rootScope', function($rootScope) {
 			}
 			if (t === 0) {
 				$rootScope.noitemsDisplay = true;
-				console.log('no');
 			}
 		}
 	};
